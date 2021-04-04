ccf357407f9be634e07ddddcc9197a71e0fe9b09
Javier Chavez <javier120699lili@gmail.com>
2021-04-03 21:07:38 -0500

    Updated format command

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const {config} = require('./config/index')
-const app = express()
+const express = require("express");
+const { config } = require("./config/index");
+const app = express();
 
-app.get("/",function (req,res) {
-  res.send('Hello world')
-})
+app.get("/", function (req, res) {
+    res.send("Hello world");
+});
 
-app.get('/json',function (req,res) {
-  res.json({hello:'wordl'})
-})
+app.get("/json", function (req, res) {
+    res.json({ hello: "world" });
+});
 
-app.listen(config.port,()=>{
-  console.log(`Listening http://localhost:${config.port}`);
-})
+app.listen(config.port, () => {
+    console.log(`Listening http://localhost:${config.port}`);
+});
