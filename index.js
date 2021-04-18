e56b8426dff135786534680843678704689b806e
Javier Chavez <javier120699lili@gmail.com>
2021-04-18 18:50:17 -0500

    Instaling cors to the project

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const { config } = require("./app/config/index");
 const InitRoutes = require("./app/router");
 const SetMiddlewares = require("./app/middlewares");
 const { json } = require("body-parser");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(json());
 
 app.get("/", function (req, res) {
